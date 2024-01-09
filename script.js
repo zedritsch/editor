24f3112489d8ba7b1289d87655f2fe0f4bde7820
Zedritsch <cedric.buttkus@gmail.com>
2024-01-09 13:10:03 +0100

    Changed layout

--- script.js
+++ script.js
@@ -1,40 +1,28 @@
-function toggleFullscreen() {
-	/** @type {HTMLImageElement} */
-	const ICON = document.getElementById("maximize");
-
-	if (document.fullscreenElement) {
-		document.exitFullscreen();
-		ICON.src = "img/open_fullscreen.svg";
-
-		return;
-	}
-
-	document.getElementsByTagName("html")[0].requestFullscreen();
-	ICON.src = "img/close_fullscreen.svg";
-}
+const nav_tabs = document.getElementsByTagName("a");
+const sidebars = document.getElementsByTagName("aside");
 
 /** @param {number} index */
 function switchSidebar(index) {
-	const NAV = document.getElementsByTagName("nav")[0].children;
-	const ASIDE = document.getElementsByTagName("aside")[0];
+	for (let i = 0; i < 3; i++) {
+		nav_tabs[i].className = "";
+		sidebars[i + 1].className = "hidden";
+	}
 
-	if (NAV.item(index).className == "active") {
-		ASIDE.className = "hidden";
-		NAV.item(index).className = "";
+	nav_tabs[index].className = "active";
+	sidebars[index + 1].className = "";
+}
+
+function toggleFullscreen() {
+	/** @type {HTMLImageElement} */
+	const icon = document.getElementById("maximize");
+
+	if (document.fullscreenElement) {
+		document.exitFullscreen();
+		icon.src = "img/open_fullscreen.svg";
 
 		return;
 	}
 
-	for (let i = 0; i < NAV.length && i < ASIDE.children.length; i++) {
-		if (i == 2) {
-			continue;
-		}
-
-		NAV.item(i).className = "";
-		ASIDE.children.item(i).className = "hidden";
-	}
-
-	ASIDE.children.item(index).className = "";
-	NAV.item(index).className = "active";
-	ASIDE.className = "";
+	document.documentElement.requestFullscreen();
+	icon.src = "img/close_fullscreen.svg";
 }
